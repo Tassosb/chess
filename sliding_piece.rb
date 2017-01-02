1de940ee2896aabe5d053fc51050d3c655e05ba1
Tassos Bareiss <tassosbareiss@gmail.com>
2017-01-02 18:51:48 -0500

    Create move method that returns all available moves

---
+++ sliding_piece.rb
@@ -0,0 +1,37 @@
+module SlidingPiece
+  def moves(available_dirs)
+    possible_moves = []
+
+    deltas = move_dirs(available_dirs)
+
+    deltas.each do |delta|
+      d_x, d_y = delta
+      new_pos = [pos[0] + d_x, pos[1] + d_y]
+
+      until !board.in_bounds?(new_pos) || !board[new_pos].is_a?(NullPiece)
+        possible_moves << new_pos
+        new_pos = [new_pos[0] + d_x, new_pos[1] + d_y]
+      end
+    end
+    #
+    # if board.in_bounds?(new_pos)
+    #   possible_moves << new_pos
+    # end
+    possible_moves
+  end
+
+  def move_dirs(available_dirs)
+    directions = {
+      flats: [[0, 1], [0, -1], [1, 0], [-1, 0]],
+      diagonal: [[1, 1], [1, -1], [-1, 1], [-1, -1]]
+    }
+
+    deltas = []
+
+    available_dirs.each do |dir|
+      deltas.concat(directions[dir])
+    end
+
+    deltas
+  end
+end
