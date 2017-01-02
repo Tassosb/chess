6c2576b9ffe05e622951b9d4e5b6ebfcfd7718b9
Tassos Bareiss <tassosbareiss@gmail.com>
2017-01-02 17:21:41 -0500

    Create to_s methods for each piece

--- piece.rb
+++ piece.rb
@@ -1,30 +1,53 @@
 class Piece
+  attr_reader :color
 
   def initialize(color)
     @color = color
   end
-
 end
 
 class Rook < Piece
+
+  def to_s
+    color == :white ? " \u2656 " : " \u265c "
+  end
 end
 
 class Knight < Piece
+  def to_s
+    color == :white ? " \u2658 " : " \u265e "
+  end
 end
 
 class Bishop < Piece
+  def to_s
+    color == :white ? " \u2657 " : " \u265d "
+  end
 end
 
 class King < Piece
+  def to_s
+    color == :white ? " \u2654 " : " \u265a "
+  end
 end
 
 class Queen < Piece
+  def to_s
+    color == :white ? " \u2655 " : " \u265b "
+  end
 end
 
 class Pawn < Piece
+  def to_s
+    color == :white ? " \u2659 " : " \u265f "
+  end
 end
 
 class NullPiece < Piece
   def initialize
+  end
+
+  def to_s
+    "   "
   end
 end
