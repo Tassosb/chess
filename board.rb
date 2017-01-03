b0d3d536af408807f34794017940cef5e79afeea
Tassos Bareiss <tassosbareiss@gmail.com>
2017-01-03 18:02:30 -0500

    Make setup and find king private methods

--- board.rb
+++ board.rb
@@ -22,32 +22,6 @@ class Board
     @grid[row][col] = mark
   end
 
-  def setup_pieces
-    color = :black
-    (0..7).each do |row_idx|
-      color = :white if row_idx > 5
-      case row_idx
-      when 0, 7
-        self[[row_idx, 0]] = Rook.new([row_idx, 0], color, self)
-        self[[row_idx, 1]] = Knight.new([row_idx, 1], color, self)
-        self[[row_idx, 2]] = Bishop.new([row_idx, 2], color, self)
-        self[[row_idx, 3]] = Queen.new([row_idx, 3], color, self)
-        self[[row_idx, 4]] = King.new([row_idx, 4], color, self)
-        self[[row_idx, 5]] = Bishop.new([row_idx, 5], color, self)
-        self[[row_idx, 6]] = Knight.new([row_idx, 6], color, self)
-        self[[row_idx, 7]] = Rook.new([row_idx, 7], color, self)
-      when 1, 6
-        8.times do |i|
-          self[[row_idx, i]] = Pawn.new([row_idx, i], color, self)
-        end
-      else
-        8.times do |i|
-          self[[row_idx, i]] = NullPiece.instance
-        end
-      end
-    end
-  end
-
   def move_piece!(start_pos, end_pos)
     self[end_pos] = self[start_pos]
     self[start_pos] = NullPiece.instance
@@ -83,18 +57,6 @@ class Board
     end
   end
 
-  def find_king_pos(color)
-    grid.each_with_index do |row, row_idx|
-      row.each_with_index do |piece, col_idx|
-        if piece.is_a?(King) && piece.color == color
-          return [row_idx, col_idx]
-        end
-      end
-    end
-
-    raise MissingKingError, "The King has disappeared!"
-  end
-
   def checkmate?(color)
     return false unless in_check?(color)
 
@@ -121,6 +83,50 @@ class Board
     end
 
     duped_board
+  end
+
+  def matches_color?(pos, color)
+    self[pos].color == color
+  end
+
+  private
+
+  def setup_pieces
+    color = :black
+    (0..7).each do |row_idx|
+      color = :white if row_idx > 5
+      case row_idx
+      when 0, 7
+        self[[row_idx, 0]] = Rook.new([row_idx, 0], color, self)
+        self[[row_idx, 1]] = Knight.new([row_idx, 1], color, self)
+        self[[row_idx, 2]] = Bishop.new([row_idx, 2], color, self)
+        self[[row_idx, 3]] = Queen.new([row_idx, 3], color, self)
+        self[[row_idx, 4]] = King.new([row_idx, 4], color, self)
+        self[[row_idx, 5]] = Bishop.new([row_idx, 5], color, self)
+        self[[row_idx, 6]] = Knight.new([row_idx, 6], color, self)
+        self[[row_idx, 7]] = Rook.new([row_idx, 7], color, self)
+      when 1, 6
+        8.times do |i|
+          self[[row_idx, i]] = Pawn.new([row_idx, i], color, self)
+        end
+      else
+        8.times do |i|
+          self[[row_idx, i]] = NullPiece.instance
+        end
+      end
+    end
+  end
+
+  def find_king_pos(color)
+    grid.each_with_index do |row, row_idx|
+      row.each_with_index do |piece, col_idx|
+        if piece.is_a?(King) && piece.color == color
+          return [row_idx, col_idx]
+        end
+      end
+    end
+
+    raise MissingKingError, "The King has disappeared!"
   end
 end
 
